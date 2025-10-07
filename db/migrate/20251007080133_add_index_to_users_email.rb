aa64c1b3abfc27c65e37733fd774a3636b7c6e8f
maha <mahalingaprabu111@gmail.com>
2025-10-07 13:37:50 +0530

    db: make add_index_to_users_email idempotent (check index_exists?)

--- db/migrate/20251007080133_add_index_to_users_email.rb
+++ db/migrate/20251007080133_add_index_to_users_email.rb
@@ -1,5 +1,7 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[8.0]
   def change
-    add_index :users, :email, unique: true
+    unless index_exists?(:users, :email)
+      add_index :users, :email, unique: true
+    end
   end
 end
